76ad485c10524400a98380f9f8957b95a9f1cb3e
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-23 14:06:02 -0600

    added links library

---
+++ lib/links.ts
@@ -0,0 +1,6 @@
+ export const links = [
+    { href: '/', label: 'Home' },
+    { href: '/projects', label: 'Projects' },
+    { href: '/skills', label: 'Skills' },
+    { href: '/contact', label: "Contact" },
+];
