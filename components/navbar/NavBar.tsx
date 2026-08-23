b4b4a50e4b0d01329596de939b79f2c2ca3ce48d
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-22 23:28:10 -0600

    planning the contents

---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+
+  const links = [
+    { href: '/', label: 'Home' },
+    { href: '/projects', label: 'Projects' },
+    { href: '/skills', label: 'Skills' },
+    { href: '/contact', label: "Contact" },
+  ];
+
+  return (
+    <header>
+      
+    </header>
+  )
+}
+
+export default NavBar
