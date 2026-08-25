b8206c16daec20d5e464cf4c990a3e0f1f60bbb9
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-25 15:08:07 -0600

    adjusted layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       
       <body className="min-h-full flex flex-col">
         <NavBar/>
-        <main>
+        <main className="pt-20">
           {children}
         </main>
         <Footer/>
