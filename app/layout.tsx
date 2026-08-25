25a4951531ed9db7518500396ae86a7df0101361
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-25 06:05:07 -0600

    fixed layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <main>
           {children}
         </main>
-        </body>
+        <Footer/>
+      </body>
     </html>
   );
 }
