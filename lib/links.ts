fcceb8f44309756485c902ff9f5b43957394f3a4
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-25 00:03:29 -0600

    updated links for a different navigation setup

--- lib/links.ts
+++ lib/links.ts
@@ -1,6 +1,6 @@
  export const links = [
-    { href: '/', label: 'Home' },
-    { href: '/projects', label: 'Projects' },
-    { href: '/skills', label: 'Skills' },
-    { href: '/contact', label: "Contact" },
+  { href: '#home', label: 'Home' },
+  { href: '#projects', label: 'Projects' },
+  { href: '#skills', label: 'Skills' },
+  { href: '#contact', label: 'Contact' },
 ];
